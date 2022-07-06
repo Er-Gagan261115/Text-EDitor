e0e2c3efebae6e4f4fd7a7d4f8a62a537317db27
Gagan Singh <gagan261115@gmail.com>
2022-07-06 23:43:38 +0530

    Updates

---
+++ precache-manifest.ce5ffeccb3ce7850371c4692bbc05bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "507f2c5e01fe54e4c617",
+    "url": "/Text-EDitor/static/css/main.732d1065.chunk.css"
+  },
+  {
+    "revision": "507f2c5e01fe54e4c617",
+    "url": "/Text-EDitor/static/js/main.507f2c5e.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Text-EDitor/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Text-EDitor/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "9a36f1ef5f2a73475ada",
+    "url": "/Text-EDitor/static/js/runtime~main.9a36f1ef.js"
+  },
+  {
+    "revision": "b3ae8e83768eb0314aa32a06fbced50c",
+    "url": "/Text-EDitor/index.html"
+  }
+];
